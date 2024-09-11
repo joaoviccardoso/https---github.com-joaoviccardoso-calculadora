6a9db42efd1e6fe7ed52306627ab4a772549c47d
joao victor da silva cardoso <pealq63@gmail.com>
2024-09-11 14:43:52 -0300

    criado os calculos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const btnsNumeros = document.querySelectorAll('.btnNumeros');
 const containerResuldado = document.querySelector('.container__resuldado');
 const btnLimpar = document.getElementById('botao-ce');
 const btnsCalculo = document.querySelectorAll('.btnCalculo');
+const btnCalcular = document.querySelector('#botao-calcular');
+console.log(btnsNumeros)
 
 let n1 = "";
 let n2 = "";
@@ -26,16 +28,40 @@ btnsNumeros.forEach(btn =>{
 btnsCalculo.forEach(btn =>{
     btn.addEventListener('click', () =>{
       const idDobtnCalcular = btn.getAttribute('id');
-      const simboloDoCalculo = idDobtnCalcular.split('-')[1];
+      const simboloDoCalculo = idDobtnCalcular.split('_')[1];
+      calculoSerFeito = simboloDoCalculo
       containerResuldado.innerHTML +=  `<h2 class="calculo">${simboloDoCalculo}</h2>`
     })
 })
 
-
+btnCalcular.addEventListener('click', () =>{
+    numeroDaSoma1 = parseFloat(n1);
+    numeroDaSoma2 = parseFloat(n2);
+    console.log(n1, n2, calculoSerFeito)
+    containerResuldado.innerHTML = `<h2 class="calculo"></h2>`;
+    
+    switch (calculoSerFeito){
+        case "+": 
+            containerResuldado.innerHTML =  `<h2 class="calculo">${n1} + ${n2}=${numeroDaSoma1+numeroDaSoma2}</h2>`;
+            break;
+        case "-":
+            containerResuldado.innerHTML =  `<h2 class="calculo">${n1} - ${n2}=${numeroDaSoma1-numeroDaSoma2}</h2>`;
+            break;
+        case "*":
+            containerResuldado.innerHTML =  `<h2 class="calculo">${n1} x ${n2}=${numeroDaSoma1*numeroDaSoma2}</h2>`;
+            break;
+        case "/":
+            containerResuldado.innerHTML =  `<h2 class="calculo">${n1} / ${n2}=${numeroDaSoma1/numeroDaSoma2}</h2>`;
+            break;
+        default:
+            containerResuldado.innerHTML = `<h2 class="calculo">Erro</h2>`
+    }
+})
 
 btnLimpar.onclick = () =>{
     containerResuldado.innerHTML = `<h2 class="calculo"></h2>`
     n1 = "";
     n2 = "";
+    calculoSerFeito = "";
 }
 
